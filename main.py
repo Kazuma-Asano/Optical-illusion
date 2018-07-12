c921864ec58294c06c60e22d4f451fee41e5abc8
Kazuma Asano <Kazuma@NaoyanoMacBook-Air.local>
2018-07-12 17:01:22 +0900

    change main.py

--- main.py
+++ main.py
@@ -13,103 +13,122 @@ import copy
 import numpy as np
 import matplotlib.pyplot as plt
 
+from utils import progress_bar
+
+import argparse
+parser = argparse.ArgumentParser(description='PyTorch Training')
+parser.add_argument('--lr', default=0.001, type=float, help='learning rate')
+parser.add_argument('--epochs', default=10, type=int, help='epochs')
+parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+args = parser.parse_args()
+
 # データセットの設定
-data_transforms = {
-    'train': transforms.Compose([
+print('==> Preparing data..')
+print('-' * 10)
+train_data_transform = transforms.Compose([
         transforms.RandomResizedCrop(224),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'val': transforms.Compose([
+    ])
+
+val_data_transform = transforms.Compose([
         transforms.Resize((256, 256)),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-}
+    ])
 
-image_datasets = {x: datasets.ImageFolder(root='./data', transform=data_transforms[x])
-                  for x in ['train', 'val']}
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x],
-                                              batch_size=4,
-                                              shuffle=True,
-                                              num_workers=4) for x in ['train', 'val']}
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
-class_names = image_datasets['train'].classes
+trainset = datasets.ImageFolder(root='./data/train', transform=train_data_transform)
+trainloader = torch.utils.data.DataLoader(trainset,
+                                            batch_size=4,
+                                            shuffle=True,
+                                            num_workers=4)
+
+valset = datasets.ImageFolder(root='./data/val', transform=val_data_transform)
+valloader = torch.utils.data.DataLoader(valset,
+                                            batch_size=4,
+                                            shuffle=False,
+                                            num_workers=4)
+class_names = trainset.classes
+print()
 
 #GPUの設定
 use_gpu = torch.cuda.is_available()
+print('use gpu: ', use_gpu)
+print('-' * 10)
+print()
 
-# Train & Test
-def train_model(model, criterion, optimizer, scheduler, num_epochs):
-    since = time.time()
+####### TRAIN #########
+def train(model, criterion, optimizer, scheduler):
+    model.train() #drop outを適用
+    train_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (data, labels) in enumerate(trainloader):
 
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_acc = 0.0
+        if use_gpu: #GPUが使えるなら
+            inputs = Variable(data.cuda())
+            labels = Variable(labels.cuda())
+        else:
+            inputs, labels = Variable(data), Variable(labels)
 
-    for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        print('-' * 10)
+        optimizer.zero_grad()
 
-        # 各エポックで訓練+バリデーションを実行
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                scheduler.step()
-                model.train(True)   # training mode
+        # forward
+        outputs = model(inputs)
+        _, preds = torch.max(outputs.data, 1)
+        loss = criterion(outputs, labels)
+
+        loss.backward() #Back propagation
+        #optimizer.step() # n epoch でlearning rate を m倍する
+
+        train_loss += loss.item()
+        total += labels.size(0)
+        correct += preds.eq(labels).sum().item()
+
+        progress_bar(batch_idx, len(trainloader),
+                    'Train Loss: {:.3f} | Train Acc: {:.3f} % ({}/{})'.format(train_loss/(batch_idx+1),
+                                                                            100.*correct/total,
+                                                                            correct, total))
+
+    print('Train Loss: {:.4f}, Train Acc: {:.4f} %'.format(train_loss/(batch_idx+1), 100.*correct/total))
+
+######## TEST ########
+def test(model):
+    model.eval() #drop outを適用しない
+    test_loss = 0.0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (data, labels) in enumerate(valloader):
+
+            if use_gpu:
+                inputs = Variable(data.cuda())
+                labels = Variable(labels.cuda())
             else:
-                model.train(False)  # evaluate mode
+                inputs, labels = Variable(data), Variable(labels)
 
-            running_loss = 0.0
-            running_corrects = 0
+            # forward
+            outputs = model(inputs)
+            _, preds = torch.max(outputs.data, 1)
+            loss = criterion(outputs, labels)
 
-            for data in dataloaders[phase]:
-                inputs, labels = data
+            # statistics
+            test_loss += loss.item()
+            total += labels.size(0)
+            correct += preds.eq(labels).sum().item()
 
-                if use_gpu:
-                    inputs = Variable(inputs.cuda())
-                    labels = Variable(labels.cuda())
-                else:
-                    inputs, labels = Variable(inputs), Variable(labels)
+            progress_bar(batch_idx, len(valloader),
+                            'Test Loss: %.3f | Test Acc: %.3f%% (%d/%d)'
+                            % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+    # 精度が改善したらモデルを保存
+    acc = 100.*correct/total
+    return acc
 
-                optimizer.zero_grad()
+    #print('Test Loss: {:.4f}, Test Acc: {:.4f}'.format(epoch_loss, epoch_acc))
 
-                # forward
-                outputs = model(inputs)
-                _, preds = torch.max(outputs.data, 1)
-                loss = criterion(outputs, labels)
-
-                if phase == 'train':
-                    loss.backward()
-                    optimizer.step()
-
-                # statistics
-                running_loss += loss.data[0] * inputs.size(0)
-                running_corrects += preds.eq(labels.data).sum().item()
-                #running_corrects += torch.sum(preds == labels.data)
-
-            # サンプル数で割って平均を求める
-            epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_acc = running_corrects / dataset_sizes[phase]
-
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
-
-            # deep copy the model
-            # 精度が改善したらモデルを保存する
-            if phase == 'val' and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model_wts = copy.deepcopy(model.state_dict())
-
-        print()
-
-    time_elapsed = time.time() - since
-    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
-    print('Best val acc: {:.4f}'.format(best_acc))
-
-    # load best model weights
-    model.load_state_dict(best_model_wts)
-    return model
-
+###### Show Result #######
 def imshow(images, title=None):
     images = images.numpy().transpose((1, 2, 0))  # (h, w, c)
     mean = np.array([0.485, 0.456, 0.406])
@@ -120,12 +139,11 @@ def imshow(images, title=None):
     if title is not None:
         plt.title(title)
 
-
 def visualize_model(model, num_images=6):
     images_so_far = 0
     fig = plt.figure()
-
-    for i, data in enumerate(dataloaders['val']):
+    model.eval()
+    for i, data in enumerate(valloader):
         inputs, labels = data
         if use_gpu:
             inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
@@ -143,56 +161,89 @@ def visualize_model(model, num_images=6):
             imshow(inputs.cpu().data[j])
 
             if images_so_far == num_images:
+                plt.show()
                 return
 
 ##################### MAIN ########################
 #####################
-##### modelの設定 #####
+##### modelの設定 ####
 #####################
-# model_ft = models.resnet18(pretrained=True)
-# num_features = model_ft.fc.in_features
-# print(num_features)  # 512
+print('==> Building model..')
+print('-' * 10)
+model = models.resnet18(pretrained=False)
+num_features = model.fc.in_features
 
 # fc層を置き換える
-# model_ft.fc = nn.Linear(num_features, 2)
-# print(model_ft)
-
-# if use_gpu:
-#     model_ft = model_ft.cuda()
-#
-# criterion = nn.CrossEntropyLoss()
-# optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
-# exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
-#
-# model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=100)
-# torch.save(model_ft.state_dict(), 'model_ft.pkl')
+model.fc = nn.Linear(num_features, 2)
+print(model)
+print()
 
 #######################
-##### Fine tuning ########
+##### Fine tuning #####
 #######################
 # 訓練済みResNet18をロード
-model_conv = torchvision.models.resnet18(pretrained=True)
+# model = torchvision.models.resnet18(pretrained=True)
 
 # すべてのパラメータを固定
-for param in model_conv.parameters():
-    param.requires_grad = False
+# for param in model.parameters():
+#     param.requires_grad = False
+#
+# # 最後のfc層を置き換える
+# # これはデフォルトの requires_grad=True のままなのでパラメータ更新対象
+# num_features = model.fc.in_features
+# model.fc = nn.Linear(num_features, 2)
 
-# 最後のfc層を置き換える
-# これはデフォルトの requires_grad=True のままなのでパラメータ更新対象
-num_features = model_conv.fc.in_features
-model_conv.fc = nn.Linear(num_features, 2)
 
 if use_gpu:
-    model_conv = model_conv.cuda()
+    model = model.cuda()
 
+if args.resume:
+    # GPUで学習したモデルのロード（CPUにも使えるよう調整)
+    print('==> Resuming from checkpoint..')
+    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+    model.load_state_dict(torch.load('./checkpoint/model.pkl',
+                            map_location=lambda storage,
+                            loc: storage))
+
+# optimaizerなどの設定
 criterion = nn.CrossEntropyLoss()
-# Optimizerの第1引数には更新対象のfc層のパラメータのみ指定
-optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
+optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
+# optimizer = optim.SGD(model.fc.parameters(), lr=0.001, momentum=0.9)　# Optimizerの第1引数には更新対象のfc層のパラメータのみ指定
 # 7epochごとにlearning rateを0.1倍する
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 
-model_conv = train_model(model_conv, criterion, optimizer_conv,
-                         exp_lr_scheduler, num_epochs=10)
+best_model_wts = copy.deepcopy(model.state_dict())
+best_acc = 0.0
 
-visualize_model(model_conv)
+# Train & Test
+since = time.time()
+print('==> Start training..')
+for epoch in range(1, args.epochs + 1):
+    print('-' * 10)
+    print('Epoch {}/{}'.format(epoch, args.epochs))
+    print('-' * 10)
+    train(model, criterion, optimizer, exp_lr_scheduler)
 
+    print('Strat Test')
+    print('-' * 10)
+    epoch_acc = test(model)
+
+    # 精度が改善したらモデルを保存する
+    if epoch_acc > best_acc:
+        best_acc = epoch_acc
+        best_model_wts = copy.deepcopy(model.state_dict())
+
+    model.load_state_dict(best_model_wts)
+    print()
+
+
+time_elapsed = time.time() - since
+print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+print('Best val acc: {:.4f}'.format(best_acc))
+
+# パラメータのセーブ
+if not os.path.isdir('checkpoint'):
+    os.mkdir('checkpoint')
+torch.save(model.state_dict(), './checkpoint/model.pkl')
+
+visualize_model(model)
